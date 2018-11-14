c1275d20452c6f35b1dc810dcac18bea42b9bb07
zhangpc <zhangpc@tenxcloud.com>
2018-11-14 11:07:57 +0800

    [KK-692] init app-stack

---
+++ src/containers/AppStack/index.js
@@ -0,0 +1,19 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2018 TenxCloud. All Rights Reserved.
+ */
+
+/**
+ * AppStack
+ *
+ * @author zhangpc
+ * @date 2018-11-14
+ */
+
+import React from 'react'
+
+export default class AppStack extends React.Component {
+  render() {
+    return <h1>AppStack</h1>
+  }
+}
