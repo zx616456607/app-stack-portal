ce342d1b1fd9d8de6c947732bfd23e0b9bae4401
zhangtao <zhangtao@tenxcloud.com>
2018-11-30 16:35:51 +0800

    [ KK-1119] 【前端任务】- sample优化

--- src/models/createNativeResource.ts
+++ src/models/createNativeResource.ts
@@ -30,7 +30,109 @@ interface YamlValuePar {
 export default {
   namespace: 'createNative',
   state: {
-    yamlValue: ``,
+    yamlValue: `
+    apiVersion: v1
+    kind: Service
+    metadata:
+      name: nginx
+      labels:
+        app: nginx
+    spec:
+      ports:
+      - port: 80
+        name: web
+      clusterIP: None
+      selector:
+        app: nginx
+    ---
+    apiVersion: apps/v1
+    kind: StatefulSet
+    metadata:
+      name: web
+    spec:
+      selector:
+        matchLabels:
+          app: nginx    # has to match .spec.template.metadata.labels
+      serviceName: "nginx"
+      replicas: 2   # by default is 1
+      template:
+        metadata:
+          labels:
+            app: nginx   # has to match .spec.selector.matchLabels
+        spec:
+          terminationGracePeriodSeconds: 10
+          containers:
+          - name: nginx
+            image: nginx:v1.9.2
+            ports:
+            - containerPort: 80
+              name: web
+            volumeMounts:
+            - name: www
+              mountPath: /usr/share/nginx/html
+      volumeClaimTemplates:
+      - metadata:
+          name: www
+        spec:
+          accessModes: [ "ReadWriteOnce" ]
+          storageClassName: "my-storage-class"
+          resources:
+            requests:
+              storage: 1Gi
+    ---
+
+    kind: Service
+    apiVersion: v1
+    metadata:
+      name: my-service
+    spec:
+      selector:
+        app: nginx
+      ports:
+      - protocol: TCP
+        port: 80
+        targetPort: 9376
+    ---
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: nginx-deployment
+    spec:
+      selector:
+        matchLabels:
+          app: nginx
+      replicas: 2
+      template:
+        metadata:
+          labels:
+            app: nginx
+        spec:
+          containers:
+          - name: nginx
+            image: nginx
+            ports:
+            - containerPort: 80
+    ---
+    apiVersion: batch/v1
+    kind: Job
+    metadata:
+      name: process-item
+      labels:
+        jobgroup: jobexample
+    spec:
+      completions: 4
+      parallelism: 2
+      template:
+        metadata:
+          name: jobexample
+          labels:
+            jobgroup: jobexample
+        spec:
+          containers:
+          - name: c
+            image: busybox
+            command: ["sh", "-c", "echo Processing item $ITEM && sleep 5"]
+          restartPolicy: Never`,
   },
   reducers: {
     updateYamlValue(state, { payload: { yamlValue = '' } = {} }: YamlValuePar) {
