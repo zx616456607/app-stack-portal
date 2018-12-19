ca79e21b2edfafce82eaac87c4585b68f279cf8a
zhangtao <zhangtao@tenxcloud.com>
2018-12-19 12:13:11 +0800

    [KK-1736] 工作负载，yaml内容为空，保存为编排文件时，保存失败，没有提示具体原因

--- src/containers/CreateWorkLoad/exportComposeFile.tsx
+++ src/containers/CreateWorkLoad/exportComposeFile.tsx
@@ -56,7 +56,7 @@ class ExportComposeFile extends React.Component<FileProps, FileState> {
       try {
         await this.props.dispatch({ type: 'createNative/createStack', payload: { obj } })
       } catch (e) {
-        return notification.warn({ message: '创建失败', description: '' })
+        return notification.warn({ message: '创建失败', description: e.response.message })
       }
       notification.success({ message: '创建成功', description: '' })
       this.props.setVisible(false)
